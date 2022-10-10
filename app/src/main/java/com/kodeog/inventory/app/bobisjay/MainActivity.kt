7363c4f5c97a726641114035abf632fcc051e45a
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-10-10 10:25:21 +0800

    add splash screen

--- app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
+++ app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
@@ -8,26 +8,10 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var username: TextView
-    private lateinit var password: TextView
-    private lateinit var login: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        username = findViewById(R.id.etUsername)
-        password = findViewById(R.id.etPassword)
-        login = findViewById(R.id.btSignIn)
-
-        login.setOnClickListener {
-            if (username.getText().equals("admin")) {
-                if (password.getText().equals("admin")) {
-                    Toast.makeText(this, "Log in Successfull", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "INVALID!", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
