e3eaa11df8b294840d49507a60a073f800bf1ab0
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-10-14 13:28:39 +0800

    login function

--- app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
+++ app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
@@ -1,17 +1,46 @@
 package com.kodeog.inventory.app.bobisjay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.kodeog.inventory.app.bobisjay.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
+        binding.btSignIn.setOnClickListener{
+            //login
+            var userName : String = binding.etUsername.text.toString()
+            var password : String = binding.etPassword.text.toString()
+            checkCredentials(userName,password)
+        }
+    }
+    //                                                              boolean return it can use in testing
+    private fun checkCredentials(userName:String,password:String):Boolean{
+        val correctUserName: String = "admin"
+        val correctPassword: String = "admin123"
+        if((correctUserName == userName) && (correctPassword == password)){
+            // declaring Intent to go in activity home page.
+            val intent = Intent(this,HomeActivity::class.java)
+            // calling the intent
+            startActivity(intent)
+            // to kill an activity to reduce the running apps
+            finish()
+            Toast.makeText(applicationContext, "Welcome $correctUserName", Toast.LENGTH_SHORT).show()
+
+            return true
+        }else{
+            Toast.makeText(applicationContext, "Account Not Registered!", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
