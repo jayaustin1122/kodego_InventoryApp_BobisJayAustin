a7316d41e454c44d89f8adb77921e9a476da87f2
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-10-15 13:37:54 +0800

    Intent and Binding To get data frim another Activity

--- app/src/main/java/com/kodeog/inventory/app/bobisjay/HomeActivity.kt
+++ app/src/main/java/com/kodeog/inventory/app/bobisjay/HomeActivity.kt
@@ -2,10 +2,21 @@ package com.kodeog.inventory.app.bobisjay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kodeog.inventory.app.bobisjay.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //calling the other data in another activity
+        var name : String? = intent.getStringExtra("sampleName")
+        binding.textWelcome.text = "Welcome $name"
+        var password : String? = intent.getStringExtra("samplePassword")
+        binding.textsample.text = "Password is $password"
+
     }
 }
--- app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
+++ app/src/main/java/com/kodeog/inventory/app/bobisjay/MainActivity.kt
@@ -3,8 +3,6 @@ package com.kodeog.inventory.app.bobisjay
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import com.kodeog.inventory.app.bobisjay.databinding.ActivityMainBinding
 
@@ -28,15 +26,32 @@ class MainActivity : AppCompatActivity() {
     private fun checkCredentials(userName:String,password:String):Boolean{
         val correctUserName: String = "admin"
         val correctPassword: String = "admin123"
+
+        val correctUserName1: String = "cardo"
+        val correctPassword1: String = "cardo123"
         if((correctUserName == userName) && (correctPassword == password)){
             // declaring Intent to go in activity home page.
             val intent = Intent(this,HomeActivity::class.java)
+            // using intent to pass other data from this screen to the another screen
+            intent.putExtra("sampleName",userName)
+            intent.putExtra("samplePassword",password)
             // calling the intent
             startActivity(intent)
             // to kill an activity to reduce the running apps
             finish()
             Toast.makeText(applicationContext, "Welcome $correctUserName", Toast.LENGTH_SHORT).show()
-
+            return true
+        }else if((correctUserName1 == userName) && (correctPassword1 == password)) {
+            // declaring Intent to go in activity home page.
+            val intent = Intent(this, HomeActivity::class.java)
+            // using intent to pass other data from this screen to the another screen
+            intent.putExtra("sampleName", userName)
+            intent.putExtra("samplePassword",password)
+            // calling the intent
+            startActivity(intent)
+            // to kill an activity to reduce the running apps
+            finish()
+            Toast.makeText(applicationContext, "Welcome $userName", Toast.LENGTH_SHORT).show()
             return true
         }else{
             Toast.makeText(applicationContext, "Account Not Registered!", Toast.LENGTH_SHORT).show()
