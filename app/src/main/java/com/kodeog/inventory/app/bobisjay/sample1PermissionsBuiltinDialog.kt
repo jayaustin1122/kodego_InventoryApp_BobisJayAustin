ff6cbf784604e7da73ef4d687edbfa14ecf91d6d
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-11-06 17:53:50 +0800

    Permissions

--- app/src/main/java/com/kodeog/inventory/app/bobisjay/sample1PermissionsBuiltinDialog.kt
+++ app/src/main/java/com/kodeog/inventory/app/bobisjay/sample1PermissionsBuiltinDialog.kt
@@ -1,13 +1,19 @@
 package com.kodeog.inventory.app.bobisjay
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -18,7 +24,8 @@ import com.kodeog.inventory.app.bobisjay.databinding.ActivitySample1PermissionsB
 import com.kodeog.inventory.app.bobisjay.databinding.CustomSampleDialogBinding
 
 class sample1PermissionsBuiltinDialog : AppCompatActivity() {
-    lateinit var binding: ActivitySample1PermissionsBuiltinDialogBinding
+    private lateinit var binding: ActivitySample1PermissionsBuiltinDialogBinding
+    private val image = "https://cdn.myanimelist.net/images/characters/11/174517.jpg"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySample1PermissionsBuiltinDialogBinding.inflate(layoutInflater)
@@ -31,26 +38,34 @@ class sample1PermissionsBuiltinDialog : AppCompatActivity() {
         binding.btnBuiltInDialog.setOnClickListener() {
             showBuiltInDialog()
         }
-        binding.btnSamplePermission.setOnClickListener(){
+        binding.btnCamera.setOnClickListener(){
             showCamera()
         }
+        binding.btnGallery.setOnClickListener(){
+            showGallery()
+        }
+        binding.imgBtnCamera.setOnClickListener(){
+            showCamera()
+            binding.imgBtnCamera.setImageResource(R.drawable.ic_baseline_edit_24)
+        }
+        // to call any data image. Glide to get images in the Internet
+        Glide.with(this)
+            .load(image)
+            .into(binding.imgSelectec)
     }
 
-    private fun showCamera() {
+
+    private fun showGallery() {
         Dexter.withContext(this).withPermission(
-            Manifest.permission.CAMERA
-        ).withListener(object : PermissionListener{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object : PermissionListener {
             override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                Toast.makeText(applicationContext, "Camera Permission Granted", Toast.LENGTH_SHORT)
-                    .show()
-                //show camera
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivity(cameraIntent)
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                galleryLauncher.launch(galleryIntent)
             }
 
             override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                Toast.makeText(applicationContext, "Camera Permission Denied", Toast.LENGTH_SHORT)
-                    .show()
+                gotoSettings()
             }
 
             override fun onPermissionRationaleShouldBeShown(
@@ -61,6 +76,48 @@ class sample1PermissionsBuiltinDialog : AppCompatActivity() {
             }
 
         }).onSameThread().check() // to show the permission
+    }
+    // to access camera.
+    private fun showCamera() {
+        Dexter.withContext(this).withPermission(
+            Manifest.permission.CAMERA
+        ).withListener(object : PermissionListener{
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                Toast.makeText(applicationContext, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+                //show camera
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                //declare the camera
+
+                cameraLauncher.launch(cameraIntent)
+//                startActivity(cameraIntent) // to go to camera only we cannot get an output or the image.
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                Toast.makeText(applicationContext, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+                gotoSettings()
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                request : PermissionRequest?,
+                token: PermissionToken?
+            ) {
+                token?.continuePermissionRequest()
+            }
+
+        }).onSameThread().check() // to show the permission
+    }
+
+    private fun gotoSettings() {
+        AlertDialog.Builder(this).setMessage("Permission is Denied Go to Settings to Change it to allow.")
+            .setPositiveButton("Go to Settings"){ dialog, item ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                var uri = Uri.fromParts("package",packageName,null)
+                intent.data = uri
+                startActivity(intent)
+            }.setNegativeButton("Cancel"){ dialog, item ->
+                dialog.dismiss()
+            }.show()
+
     }
 
     // Built in Dialog
@@ -92,5 +149,25 @@ class sample1PermissionsBuiltinDialog : AppCompatActivity() {
             Toast.makeText(applicationContext, "Ok Enjoy", Toast.LENGTH_SHORT).show()
         }
         customDialog.show()
+    }
+    // handle data or images from the camera
+    val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.extras.let {
+                val image : Bitmap = result.data?.extras?.get("data") as Bitmap // get only the image.
+                binding.imgSelectec.setImageBitmap(image)
+            }
+        }
+
+    }
+    // handle data or images in Gallery
+    val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.let {
+                val selectedImage = result.data?.data
+                binding.imgSelectec.setImageURI(selectedImage)
+            }
+
+        }
     }
 }
